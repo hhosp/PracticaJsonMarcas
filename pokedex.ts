944d1f7b2fd9bbbec5f056d1209ed8c126b2fe7a
hhosp <148861757+hhosp@users.noreply.github.com>
2025-03-11 10:34:03 +0100

    pokedex.ts 1.0 Hector

--- pokedex.ts
+++ pokedex.ts
@@ -1,4 +1,16 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=649'
+//1ra versió de la pokedex on a traves d'una funció map retornem els noms dels pokemons al url de la api a la consola
+const URL2 = 'https://pokeapi.co/api/v2/pokemon?limit=649';
 
-const searchBar = document.getElementById("search");
-const searchButton = document.getElementById("search-button");
+async function fetchData() {
+  try {
+    const response = await fetch(URL2);
+    const data = await response.json();
+    const pokemons = data.results;
+    const pokemonNames = pokemons.map(pokemon => pokemon.name);
+    console.log(pokemonNames);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+fetchData();
