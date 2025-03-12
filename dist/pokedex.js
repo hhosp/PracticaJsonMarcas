a62201e7e2e37f4c9566b828fb6f75883fa7de4f
hhosp <hhospitalmoros@gmail.com>
2025-03-12 21:08:15 +0100

    1ra versió de implementació del buscador amb resultats, falten comentaris, Héctor

---
+++ dist/pokedex.js
@@ -0,0 +1,111 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var URL2 = 'https://pokeapi.co/api/v2/pokemon/';
+var searchInput = document.getElementById("search");
+var pokemonContainer = document.getElementById("pokemon-container");
+var searchButton = document.getElementById("search-button");
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, data, pokemons, pokemonNames, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch(URL2 + '?limit=1000')];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    pokemons = data.results;
+                    pokemonNames = pokemons.map(function (pokemon) { return pokemon.name; });
+                    console.log(pokemonNames);
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.error('Error:', error_1);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+fetchData();
+function searchPokemon() {
+    return __awaiter(this, void 0, void 0, function () {
+        var searchedPokemon, response, data, error_2;
+        var _a, _b;
+        return __generator(this, function (_c) {
+            switch (_c.label) {
+                case 0:
+                    searchedPokemon = searchInput === null || searchInput === void 0 ? void 0 : searchInput.value.toLowerCase();
+                    _c.label = 1;
+                case 1:
+                    _c.trys.push([1, 4, , 5]);
+                    return [4 /*yield*/, fetch("".concat(URL2, "/").concat(searchedPokemon))];
+                case 2:
+                    response = _c.sent();
+                    return [4 /*yield*/, response.json()];
+                case 3:
+                    data = _c.sent();
+                    if (pokemonContainer) {
+                        pokemonContainer.innerHTML =
+                            "\n        <h3>".concat(data.name, "</h3>\n        <p>Numero: ").concat(data.id, "</p>\n        <p>Tipos: ").concat(data.types[0].type.name, " ").concat((_b = (_a = data.types[1]) === null || _a === void 0 ? void 0 : _a.type.name) !== null && _b !== void 0 ? _b : '', "</p>\n        <img src=\"").concat(data.sprites.front_default, "\">\n      ");
+                    }
+                    else {
+                        console.error('Pokemon Container is null');
+                    }
+                    return [3 /*break*/, 5];
+                case 4:
+                    error_2 = _c.sent();
+                    console.error(error_2);
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
+}
+//Event listener per a trucar la funció searchPokemon si es dona click en el botó de buscar
+searchButton === null || searchButton === void 0 ? void 0 : searchButton.addEventListener("click", searchPokemon);
+//Event listener per a trucar la funció e si es presiona una tecla
+searchInput === null || searchInput === void 0 ? void 0 : searchInput.addEventListener("keypress", function (e) {
+    //Si la tecla presionada es Enter es truca a la funcio searchPokemon
+    if (e.key === "Enter") {
+        searchPokemon();
+    }
+});
--- pokedex.ts
+++ pokedex.ts
@@ -1,11 +1,32 @@
-//1ra versió de la pokedex on a traves d'una funció map retornem els noms dels pokemons al url de la api a la consola
-const URL2 = 'https://pokeapi.co/api/v2/pokemon?limit=649';
+const URL2 = 'https://pokeapi.co/api/v2/pokemon/';
+
+const searchInput = document.getElementById("search") as HTMLInputElement;
+const pokemonContainer = document.getElementById("pokemon-container");
+const searchButton = document.getElementById("search-button");
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonData {
+  name: string;
+  id: number;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    type: {
+      name: string
+    }
+  }[]
+}
 
 async function fetchData() {
   try {
-    const response = await fetch(URL2);
+    const response = await fetch(URL2+'?limit=1000');
     const data = await response.json();
-    const pokemons = data.results;
+    const pokemons: Pokemon[] = data.results;
     const pokemonNames = pokemons.map(pokemon => pokemon.name);
     console.log(pokemonNames);
   } catch (error) {
@@ -14,3 +35,33 @@ async function fetchData() {
 }
 
 fetchData();
+
+async function searchPokemon() {
+  const searchedPokemon = searchInput?.value.toLowerCase();
+  try {
+    const response = await fetch(`${URL2}/${searchedPokemon}`);
+    const data: PokemonData = await response.json();
+    if (pokemonContainer) {
+      pokemonContainer.innerHTML = 
+      `
+        <h3>${data.name}</h3>
+        <p>Numero: ${data.id}</p>
+        <p>Tipos: ${data.types[0].type.name} ${data.types[1]?.type.name ?? ''}</p>
+        <img src="${data.sprites.front_default}">
+      `;
+    } else {
+      console.error('Pokemon Container is null');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+//Event listener per a trucar la funció searchPokemon si es dona click en el botó de buscar
+searchButton?.addEventListener("click", searchPokemon);
+//Event listener per a trucar la funció e si es presiona una tecla
+searchInput?.addEventListener("keypress", e => {
+  //Si la tecla presionada es Enter es truca a la funcio searchPokemon
+  if (e.key === "Enter") {
+    searchPokemon();
+  }
+});
